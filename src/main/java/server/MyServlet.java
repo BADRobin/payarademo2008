937da8a55571450f1dd6c61ef7de87db96258205
eugenyb <eberkunsky@gmail.com>
2018-08-21 16:34:26 +0300

    Initial commit

---
+++ src/main/java/server/MyServlet.java
@@ -0,0 +1,42 @@
+package server;
+
+import beans.MyBean;
+import beans.Person;
+import dao.PersonDAO;
+
+import javax.annotation.Resource;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "MyServlet", urlPatterns = {"*.html"})
+public class MyServlet extends HttpServlet {
+    @EJB
+    PersonDAO personDAO;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getRequestURI().endsWith("/hello.html")) {
+            List<Person> people = personDAO.getAllPeople();
+            request.setAttribute("mybean", new MyBean(people));
+            request.getRequestDispatcher("/hello.jsp").forward(request, response);
+        } else {
+            request.getRequestDispatcher("/error.jsp").forward(request, response);
+        }
+    }
+
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,25 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: User
+  Date: 21.08.2018
+  Time: 16:13
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>Hello from Payara Server</h1>
+    <table>
+        <c:forEach var="p" items="${mybean.people}" >
+            <tr>
+                <td>${p.id}</td><td>${p.name}</td><td>${p.age}</td>
+            </tr>
+        </c:forEach>
+    </table>
+</body>
+</html>
